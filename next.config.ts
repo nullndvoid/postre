52b2e70b7fff0c228c99adf5cccb4d5d121aa05e
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-01-05 01:24:39 +0000

    Use Chakra suggested optimisation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
