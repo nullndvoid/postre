044c4cc3866c3d5ee9e599b55d46b0c18daf7d90
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-01-07 00:24:03 +0000

    Delete Chakra UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { Provider } from "@/app/chakra-components/ui/provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>{children}</Provider>
+        {children}
       </body>
     </html>
   );
