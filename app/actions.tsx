e205319e16cb4eb4badbe1bc646386b3a5b8fa1d
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-01-09 05:00:14 +0000

    Add iron for sessions

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,11 +1,22 @@
 "use server";
 
 import { z } from "zod";
+import { cookies } from "next/headers";
+import { getIronSession } from "iron-session";
 
 const UserSchema = z.object({
   email: z.email(),
   password: z.string().min(10),
 });
+
+async function getIronSessionData() {
+  const session = await getIronSession(await cookies(), {
+    password: process.env.IRON_SESSION_SECRET!,
+    cookieName: "__irn_session",
+  });
+
+  return session;
+}
 
 export async function loginUser(_initialState: any, formData: FormData) {
   const validatedFields = UserSchema.safeParse({
